205f772127a969bab5f4ef862f1808314a7ae1ec
Sameer <contact.sameer04@gmail.com>
2023-08-27 12:01:43 +0530

    Highlight active link

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const global = {
+  currentPage: window.location.pathname,
+};
+
+//Highlight active link
+function highlightActiveLink() {
+  const links = document.querySelectorAll('.nav-link');
+  links.forEach((link) => {
+    if (link.getAttribute('href') === global.currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
+//Init App
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('Home');
+      break;
+    case '/shows.html':
+      console.log('Shows');
+      break;
+    case '/movie-details.html':
+      console.log('Movies-details');
+      break;
+    case '/tv-details.html':
+      console.log('Tv-details');
+      break;
+    case '/search.html':
+      console.log('search');
+      break;
+  }
+
+  highlightActiveLink();
+}
+
+document.addEventListener('DOMContentLoaded', init);
